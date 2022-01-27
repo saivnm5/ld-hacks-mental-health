450842a8ff42eefd31018814af2ea1e4cbe05b1f
Adam Winick <adam.winick@loblaw.ca>
2022-01-27 15:34:34 -0500

    updated style

--- src/HelpLater.jsx
+++ src/HelpLater.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Siren1 from './siren1.png'
+import Siren2 from './siren2.png'
 
 const HelpLater = () => (
   <Row >  
     <Col xs={12}>
       <Link className='' to='/help-now'> 
-        <div className='cta-container light-green'>
+        <div className='cta-container light-yellow'>
           <div>
             <p className='cta'>I need help later </p>
             <p className='description'>You can browse through our curalted list of mental health professionals, and book an appointment whenever you need.</p>
           </div>
-          <img className='icon' src={Siren1} alt="Siren" />
+          <img className='icon' src={Siren2} alt="Siren" />
         </div>
       </Link>
     </Col>
