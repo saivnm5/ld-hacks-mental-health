7afa58b5cb079f1f47fa5147bfb16c140f23248d
aravind.rajesh <aravind.rajesh@loblaw.ca>
2022-01-27 16:43:27 -0500

    emergencyhelp

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import HelpLater from './HelpLater.jsx';
 import HelpLanding from './HelpLanding.jsx';
-import HelpNow from './HelpNow.jsx';
+import Emergencyhelp from './EmergencyHelp.jsx';
+
 
 const App = () => (
   <Container className='full-height'>
     <Routes>
       <Route path="/" element={HelpLanding()} />
-      <Route path="help-now" element={HelpNow()} />
+      <Route path="help-now" element={Emergencyhelp()} />
       <Route path="help-later" element={HelpLater()} />
     </Routes>
   </Container>
--- src/EmergencyHelp.jsx
+++ src/EmergencyHelp.jsx
@@ -2,17 +2,52 @@ import React from 'react';
 import './App.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import HelpNow from './HelpNow';
-import HelpLater from './HelpLater.jsx';
+import {Link} from "react-router-dom";
+import MentalHealth from "./MentalHealth.png";
+import TalkToImg from "./TalkTo.png";
 
-const HelpLanding = () => (
+const Emergencyhelp = () => (
   <Row className='full-height'>
     <Col xs={12} className='vertical-center'>
-      <h1 className="title">What help do you need?</h1>
-      <HelpNow />
-      <HelpLater />
+      <h1 className="title">Emergency help</h1>
+      <HealthDay />
+      <TalkTo />
     </Col>
   </Row>
 );
 
-export default HelpLanding;
+const HealthDay = () => (
+    <Row >
+        <Col xs={12}>
+            <Link className='' to='/health-day'>
+                <div className='cta-container light-green'>
+                    <div>
+                        <p className='cta'>Take a mental health day </p>
+                        <p className='description'>We will inform your manager and take care of the rest!</p>
+                    </div>
+                    <img className='icon' src={MentalHealth} alt="MentalHealth" />
+                </div>
+            </Link>
+        </Col>
+    </Row>
+);
+
+const TalkTo = () => (
+    <Row >
+        <Col xs={12}>
+            <Link className='' to='/talk-to'>
+                <div className='cta-container light-brown'>
+                    <div>
+                        <p className='cta'>Talk to someone</p>
+                        <p className='description'>Small test to describe the process of taking a mental day</p>
+                    </div>
+                    <img className='icon' src={TalkToImg} alt="TalkTo" />
+                </div>
+            </Link>
+        </Col>
+    </Row>
+);
+
+
+
+export default Emergencyhelp;
