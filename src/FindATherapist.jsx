ca5c8c73cfaf4f207b0f16a0ce3564dacedd6389
Adam Winick <adam.winick@loblaw.ca>
2022-01-27 16:51:40 -0500

    css change

--- src/FindATherapist.jsx
+++ src/FindATherapist.jsx
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import {Form} from 'react-bootstrap';
 
 const FindATherapist = () => (
   <Fragment>
@@ -12,7 +13,28 @@ const FindATherapist = () => (
     </Row>
     <Row >  
       <Col xs={12}>
+        <Form>
+          <p className='form-heading'>Question 1</p>
+          <p className='form-description'>On a scale of 1 to 10 rate your mood</p>
+          <p className='form-subtext'>1 being lowest or unhappiest, 10 being highest or happiest</p>
+        </Form>
       </Col>
+    </Row>
+    <Row>
+      
+    </Row>
+    <Row >  
+      <Col xs={12}>
+        <Form>
+          <p className='form-heading'>Question 2</p>
+          <p className='form-description'>On a scale of 1 to 10 rate your mood</p>
+          <p className='form-subtext'>Select all categories that best describe the help you need.</p>
+        </Form>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={6}><label><input type="checkbox" name="checkbox" />Select me</label></Col>
+      <Col xs={6}><label><input type="checkbox" name="checkbox" />Select me</label></Col>
     </Row>
   </Fragment>
   );
